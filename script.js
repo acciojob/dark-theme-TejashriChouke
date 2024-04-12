e3d8f034100d7bb31a21e690b28f10c396fb08c2
Tejashri Rajendra Chouke <139746868+TejashriChouke@users.noreply.github.com>
2024-04-12 15:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+function swapTheme() {
+  const appDiv = document.getElementById("app");
+  const swapBtn = document.getElementById("swap");
+
+  if (appDiv.classList.contains("day")) {
+    appDiv.classList.remove("day");
+    appDiv.classList.add("night");
+    swapBtn.textContent = "Switch to Day Theme";
+  } else {
+    appDiv.classList.remove("night");
+    appDiv.classList.add("day");
+    swapBtn.textContent = "Switch to Night Theme";
+  }
+}
 
